659b40d3ac456abe69f4b6782238b42bad17ab0a
Ellen Duong <ellen@levyvlan502.0604.wlan.wireless-resnet.upenn.edu>
2017-03-17 00:42:17 -0400

    added default material, and experimental playground

---
+++ Assets/Scripts/Tri.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+// This is an experimental playground for me to to mess with GL Triangles and see how things work
+// without having to mess around a lot with the cloth simulation.
+public class Tri : MonoBehaviour {
+	public Material material;
+	public Vector3 p1;
+	public Vector3 p2;
+	public Vector3 p3;
+
+	// Use this for initialization
+	void Start () {
+		p1 = new Vector3 (0, 0, 0);
+		p2 = new Vector3 (1, 0, 0);
+		p3 = new Vector3 (1, 1, 0);
+
+		// Unity has a built-in shader that is useful for drawing
+		// simple colored things. In this case, we just want to use
+		// a blend mode that inverts destination colors.			
+		var shader = Shader.Find ("Hidden/Internal-Colored");
+		material = new Material (shader);
+		material.hideFlags = HideFlags.HideAndDontSave;
+		// Set blend mode to invert destination colors.
+		material.SetInt ("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.OneMinusDstColor);
+		material.SetInt ("_DstBlend", (int)UnityEngine.Rendering.BlendMode.Zero);
+		// Turn off backface culling, depth writes, depth test.
+		material.SetInt ("_Cull", (int)UnityEngine.Rendering.CullMode.Off);
+		material.SetInt ("_ZWrite", 0);
+		material.SetInt ("_ZTest", (int)UnityEngine.Rendering.CompareFunction.Always);
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+	}
+
+	void OnRenderObject() {
+		material.SetPass(0);
+
+		GL.PushMatrix();
+		GL.MultMatrix (transform.localToWorldMatrix);
+		GL.Begin(GL.TRIANGLES);
+
+		GL.Vertex3(p1[0], p1[1], p1[2]);
+		GL.Vertex3(p2[0], p2[1], p2[2]);
+		GL.Vertex3(p3[0], p3[1], p3[2]);
+
+		GL.End();
+		GL.PopMatrix();
+	}
+}
--- Assets/Scripts/myParticleSystem.cs
+++ Assets/Scripts/myParticleSystem.cs
@@ -50,19 +50,17 @@ namespace AssemblyCSharp
 			Vector3 p2pos = p2.getPos ();
 			Vector3 p3pos = p3.getPos ();
 
-			Debug.Log ("Triangle: " + p1pos.ToString () + " " + p2pos.ToString () + " " + p3pos.ToString ());
+			// Debug.Log ("Triangle: " + p1pos.ToString () + " " + p2pos.ToString () + " " + p3pos.ToString ());
 
-//			GL.PushMatrix();
-//			material.SetPass(0);
-//			GL.LoadOrtho();
-//			GL.Begin(GL.TRIANGLES);
 			GL.Vertex3(p1pos[0], p1pos[1], p1pos[2]);
 			GL.Vertex3(p2pos[0], p2pos[1], p2pos[2]);
 			GL.Vertex3(p3pos[0], p3pos[1], p3pos[2]);
-//			GL.End();
-//			GL.PopMatrix();
 		}
 
+		// Similar to OnPostRender
+		// OnRenderObject: https://docs.unity3d.com/ScriptReference/MonoBehaviour.OnRenderObject.html
+		// OnPostRender: https://docs.unity3d.com/ScriptReference/MonoBehaviour.OnPostRender.html
+		// Draws after the camera has finished rendering the scene
 		void OnRenderObject() {
 			material.SetPass(0);
 
@@ -70,22 +68,11 @@ namespace AssemblyCSharp
 			GL.MultMatrix (transform.localToWorldMatrix);
 			GL.Begin(GL.TRIANGLES);
 
-//			GL.Vertex3(0, 0, 0);
-//			GL.Vertex3(0, 1, 0);
-//			GL.Vertex3(1, 1, 0);
-
 			for (int i = 0; i < particleWidth - 1; i++) {
 				for (int j = 0; j < particleHeight - 1; j++) {
 					Vector3 p1pos = getParticle(i, j).getPos ();
 					Vector3 p2pos = getParticle(i + 1, j).getPos ();
-					Vector3 p3pos = getParticle(i, j + 1).getPos ();
-
-//					GL.Vertex3(p1pos[0], p1pos[1], p1pos[2]);
-//					GL.Vertex3(p2pos[0], p2pos[1], p2pos[2]);
-//					GL.Vertex3(p3pos[0], p3pos[1], p3pos[2]);
-//
-//					Debug.Log ("Triangle: " + p1pos.ToString () + " " + p2pos.ToString () + " " + p3pos.ToString ());
-//
+					Vector3 p3pos = getParticle (i, j + 1).getPos ();
 
 					drawTriangle (getParticle (i, j), getParticle (i + 1, j), getParticle (i, j + 1));
 					drawTriangle (getParticle (i + 1, j), getParticle (i + 1, j + 1), getParticle (i, j + 1));
@@ -98,8 +85,23 @@ namespace AssemblyCSharp
 
 		void Start () {
 			if (!material) {
+				Debug.Log ("here");
 				material = new Material (Shader.Find ("Diffuse"));
 			}
+
+			// Unity has a built-in shader that is useful for drawing
+			// simple colored things. In this case, we just want to use
+			// a blend mode that inverts destination colors.			
+			var shader = Shader.Find ("Hidden/Internal-Colored");
+			material = new Material (shader);
+			material.hideFlags = HideFlags.HideAndDontSave;
+			// Set blend mode to invert destination colors.
+			material.SetInt ("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.OneMinusDstColor);
+			material.SetInt ("_DstBlend", (int)UnityEngine.Rendering.BlendMode.Zero);
+			// Turn off backface culling, depth writes, depth test.
+			material.SetInt ("_Cull", (int)UnityEngine.Rendering.CullMode.Off);
+			material.SetInt ("_ZWrite", 0);
+			material.SetInt ("_ZTest", (int)UnityEngine.Rendering.CompareFunction.Always);
 
 			particles = new List<myParticle>();
 			constraints = new List<Constraint>();
